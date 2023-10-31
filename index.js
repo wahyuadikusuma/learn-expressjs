642b7892008cd7aab9d2d37a69098c6150927047
Wahyu Adi Kusuma <wahyuadikusuma@gmail.com>
2023-10-31 17:22:19 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express  from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import db from "./config/Database.js";
+import router from "./routes/index.js";
+dotenv.config();
+const app = express();
+
+try {
+    await db.authenticate();
+    console.log("Database connected..");
+    // await db.sync();
+} catch (error) {
+    console.log('Error: ',error);
+}
+
+app.use(cookieParser());
+app.use(express.json());
+// app.use(express.urlencoded({extended:true}))
+app.use(router)
+app.listen(5000, () => console.log('Server running at port 5000'));
